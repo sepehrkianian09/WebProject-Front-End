2e8beeeef755da1d2f0f8d42ba81026bbde04503
amirsadra79 <58373783+amirsadra79@users.noreply.github.com>
2020-08-28 01:22:41 +0430

    finish login - signup

--- Js Functionals/JS1.js
+++ Js Functionals/JS1.js
@@ -1,29 +1,39 @@
 const greenColor = "green"
 const whiteColor = "white"
+var onLoginPage = false;
+const loginButton = document.getElementById("LoginMenuButton")
+const signupButton = document.getElementById("SignUpMenuButton")
+const signupMenu = document.getElementById("SignUp-Menu")
+const LoginMenu = document.getElementById("Login-Menu")
+
+function loginMenu() {
+    if (onLoginPage) return;
+    onLoginPage = true;
+    change(greenColor, whiteColor, "none", "block");
+}
+
+function change(color1, color2, display1, display2) {
+    loginButton.style.backgroundColor = color1;
+    signupButton.style.backgroundColor = color2;
+    signupMenu.style.display = display1;
+    LoginMenu.style.display = display2;
+    console.log(signupMenu.style.display)
+    console.log(signupMenu)
+    console.log(LoginMenu)
+}
 
 function showLoginMenu() {
     $('.ui.modal')
         .modal('show')
     ;
-    document.getElementById("LoginMenuButton").style.backgroundColor = whiteColor;
-    // document.getElementById("SignUpMenuButton").style.backgroundColor = whiteColor;
-    showLogSinMenu(document.getElementById("LoginMenuButton"),
-        document.getElementById("SignUpMenuButton"),
-        "Login-Menu",
-        "SignUp-Menu");
+    loginMenu();
 }
 
-function showLogSinMenu(button1, button2, menu1, menu2) {
-    console.log(button1.style.backgroundColor)
-    if (button1.style.backgroundColor === whiteColor) {
-        button1.style.backgroundColor = greenColor;
-        button2.style.backgroundColor = whiteColor;
-        var menu11 = document.getElementById(menu1)
-        var menu12 = document.getElementById(menu2)
-        console.log($(menu2))
-        console.log("**********")
-        console.log($(menu1))
-        $(menu1).transition('fade right');
-        $(menu2).transition('fade left');
+function signUpMenu() {
+    if (!onLoginPage) {
+        return
     }
+    onLoginPage = false;
+    change(whiteColor, greenColor, "block", "none");
+
 }
