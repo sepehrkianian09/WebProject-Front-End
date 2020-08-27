ea211b33e535d485a6d3181e35f01d8a0373285b
Sepehrkianian09 <sepehrkianian09@yahoo.com>
2020-08-27 16:51:18 +0430

    Commit

--- JS1.js
+++ JS1.js
@@ -1,1 +1,10 @@
-console.log(jQuery().jquery);
+$('.button').click(function(){
+    var buttonId = $(this).attr('id');
+    $('#modal-container').removeAttr('class').addClass(buttonId);
+    $('body').addClass('modal-active');
+})
+
+$('#modal-container').click(function(){
+    $(this).addClass('out');
+    $('body').removeClass('modal-active');
+});
