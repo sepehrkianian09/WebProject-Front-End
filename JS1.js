340b68c5c45d6a1e3e34ea6391c6fd6f9234eda6
Sepehrkianian09 <sepehrkianian09@yahoo.com>
2020-08-27 18:51:59 +0430

    Init StackPane

--- JS1.js
+++ JS1.js
@@ -1,10 +1,5 @@
-$('.button').click(function(){
-    var buttonId = $(this).attr('id');
-    $('#modal-container').removeAttr('class').addClass(buttonId);
-    $('body').addClass('modal-active');
-})
-
-$('#modal-container').click(function(){
-    $(this).addClass('out');
-    $('body').removeClass('modal-active');
-});
+function showLoginMenu() {
+    $('.ui.modal')
+        .modal('show')
+    ;
+}
