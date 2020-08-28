8f75882b3346c165d676e1fe1d5e65d64e1dc1dc
amirsadra79 <58373783+amirsadra79@users.noreply.github.com>
2020-08-28 16:01:17 +0430

    push

--- Js Functionals/Login SignUp Method.js
+++ Js Functionals/Login SignUp Method.js
@@ -1,16 +1,97 @@
-const signUpUsername = document.getElementById("SignUp-Username")
+const urlSignUp = "https://localhost:8008/register"
+const urlLogin = "https://localhost:8008/login"
+
+
+const signUpUsername = document.getElementById("SignUp-UserName")
 const signUpFirstName = document.getElementById("SignUp-FirstName")
 const signUpLastName = document.getElementById("SignUp-LastName")
 const signUpEmail = document.getElementById("SignUp-Email")
 const signUpPassword = document.getElementById("SignUp-Password")
-const signUpRepeatUsername = document.getElementById("SignUp-RepeatPassword")
+const signUpRepeatPassword = document.getElementById("SignUp-RepeatPassword")
 const signUpIsFreeLancer = document.getElementById("isFreeLancer-ToggleButton")
-function signUp() {
 
+function signUp() {
+    var doc = hasEmpty(signUpUsername, signUpFirstName, signUpLastName, signUpEmail, signUpPassword, signUpRepeatPassword)
+    if (doc != null) {
+        doc.style.border = "1px solid red";
+        setTimeout(() => alert("fill the red box!!"), 1000);
+    } else {
+        const data = {
+            username: signUpUsername,
+            firstname: signUpFirstName,
+            lastname: signUpLastName,
+            email: signUpEmail,
+            password: signUpPassword
+        }
+        const response = fetch(urlSignUp, {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            key: signUpIsFreeLancer.value ? "freelancer" : "karfarma",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify(data)
+        }).then(successSignUp)
+            .catch(denySignUp);
+    }
 }
+
+function successSignUp(res) {
+    console.log("ok")
+}
+
+function denySignUp(res) {
+    console.log("riidii")
+}
+
 
 const loginKeypoint = document.getElementById("login-KeyPoint");
 const loginPassword = document.getElementById("login-Password");
-function login() {
 
+function login() {
+    var doc = hasEmpty(loginKeypoint, loginPassword)
+    if (doc != null) {
+        doc.style.border = "1px solid red";
+        setTimeout(() => alert("fill the red box!!"), 1000);
+    } else {
+        const data = {
+            keyPoint: loginKeypoint,
+            password: loginPassword
+        }
+        const response = fetch(urlLogin, {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify(data)
+        }).then(successLogin)
+            .catch(denyLogin);
+    }
+}
+
+function successLogin(res) {
+    console.log("success");
+}
+
+function denyLogin(res) {
+    console.log("riidiii");
+}
+
+function hasEmpty(...args) {
+    for (let doc of args) {
+        console.log(doc);
+        if (doc.value == "") {
+            return doc;
+        }
+    }
+    return null;
 }
